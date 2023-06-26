d2106168c188d7bb460dc15d0503f84b0c06c50c
Adhi Setiawan <adhisetiawan518@gmail.com>
2023-06-26 21:17:38 +0700

    adding mobilenetv2 architecture

---
+++ src/models/components/mobilenetv2.py
@@ -0,0 +1,17 @@
+from torch.utils.model_zoo import load_url as load_state_dict_from_url
+
+
+__all__ = ['MobileNetV2', 'mobilenet_v2']
+
+model_urls = {
+    'mobilenet_v2': 'https://download.pytorch.org/models/mobilenet_v2-b0353104.pth',
+}
+
+def _make_divisible(v, divisor, min_value=None):
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v

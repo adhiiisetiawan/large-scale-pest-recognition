a08ee2ea1db1f673d7cabe5c4c540ae32c5aaad5
Adhi Setiawan <51025603+adhiiisetiawan@users.noreply.github.com>
2023-07-11 23:01:26 +0700

    add sparse regularization docstring

--- pest_rec/models/components/sparse_regularization.py
+++ pest_rec/models/components/sparse_regularization.py
@@ -2,6 +2,21 @@ import torch
 import torch.nn.functional as F
 
 def sparse_loss(model, images):
+    """
+    Computes the sparse regularization loss for a given model and input images.
+
+    The sparse regularization loss encourages the activations of the model to be sparse, promoting
+    the selection of a subset of relevant features and reducing the overall complexity of the model.
+
+    Args:
+        model (torch.nn.Module): The neural network model.
+        images (torch.Tensor): The input images tensor of shape (batch_size, channels, height, width).
+
+    Returns:
+        torch.Tensor: The sparse regularization loss as a scalar tensor.
+
+    """
+    
     loss = 0
     values = images
     for module in model.children():
